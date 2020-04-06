8146eaa780453e80b06d91524f0e481e311675eb
Dario Martinez <gusdamtzr@gmail.com>
2020-04-05 19:06:40 -0500

    top practica 3

--- top.v
+++ top.v
@@ -5,12 +5,39 @@
 //////////////////////////////////////////////////////////////////////////////////
 module top(
         input clk,
-        output [6:0] disp
+        input reset,
+        input enable,
+        output [6:0] disp,
+        output [1:0] mux,
     );
 
-    wire [3:0] num;
+    wire [3:0] tenthsSec;
+    wire [3:0] unitsSec;
+    wire [3:0] tensSec;
+    wire [3:0] min;
 
-    counter c(.clk(clk), .out(num));
+    //module        input                   output
+    tenthsCouter    a(.clk(clk),            .out(tenthsCouter));
+    unitsCouter     b(.clk(tenthsCouter),   .out(unitsSec));
+    tensCouter      c(.clk(unitsSec),       .out(tensSec));
+    minsCouter      d(.clk(tensSec),        .out(min));
+
+    wire [6:0] displayTenths;
+    wire [6:0] displayUnits;
+    wire [6:0] displayTens;
+    wire [6:0] displayMins;
+
+    display d1(.n(tenthsSec),   .d(displayTenths));
+    display d1(.n(unitsSec),    .d(displayUnits));
+    display d1(.n(tensSec),     .d(displayTens));
+    display d1(.n(min),         .d(displayMins));
+
+    mux m(.clk(clk), .dout(disp), .pos(mux),
+                    .d1(displayTenths),
+                    .d2(displayUnits), 
+                    .d3(displayTens), 
+                    .d4(displayMins)
+                    );
 
     display d(.n(num), .d(disp));
                       
