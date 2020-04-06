0c8d93b037ef2d5f23623cfb5e104109ab95fd89
Dario Martinez <gusdamtzr@gmail.com>
2020-04-06 18:22:00 -0500

    CALI

--- top.v
+++ top.v
@@ -5,24 +5,26 @@
 //////////////////////////////////////////////////////////////////////////////////
 module top(
         input clk,
+        input [3:0] n1,
+        input [3:0] n2,
         output [6:0] disp,
         output [3:0] mux
     );
 
-    wire [3:0] decisecond;
-    wire [3:0] second;
-    wire [3:0] tenseconds;
-    wire [3:0] hundredseconds;
+    //wire [3:0] decisecond;
+    //wire [3:0] second;
+    //wire [3:0] tenseconds;
+    //wire [3:0] hundredseconds;
 
-    tenthsCounter c1(.clk(clk), .out(decisecond));
-    tenthsCounter c2(.clk(decisecond[0]), .out(second));
-    tenthsCounter c3(.clk(second[0]), .out(tenseconds));
-    tenthsCounter c4(.clk(tenseconds[0]), .out(hundredseconds));
+    //tenthsCounter c1(.clk(clk), .out(decisecond));
+    //tenthsCounter c2(.clk(decisecond[0]), .out(second));
+    //tenthsCounter c3(.clk(second[0]), .out(tenseconds));
+    //tenthsCounter c4(.clk(tenseconds[0]), .out(hundredseconds));
 
     mux m(.clk(clk), .dout(disp), .pos(mux),
-                    .n1(decisecond),
-                    .n2(second), 
-                    .n3(tenseconds), 
-                    .n4(hundredseconds));
+                    .n1(n1),
+                    .n2(n1), 
+                    .n3(n2), 
+                    .n4(n2));
                       
 endmodule
