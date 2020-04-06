81deeea7d27eb1c45672060f5e24c03f27a3c33f
Dario Martinez <gusdamtzr@gmail.com>
2020-04-05 23:04:51 -0500

    update

--- tenthsCounter.v
+++ tenthsCounter.v
@@ -14,36 +14,38 @@ module tenthsCounter(
 	reg [3:0] num;
     reg play;
 
-    always @ (posedge clk, posedge reset, posedge enable) begin
+    always @ (posedge clk or posedge reset or posedge enable) begin
 
         if (enable) begin
-            if (play == 0) begin
-                play <= 1;
-            end else begin
-                play <= 0;
-            end
-        end
+            case (play)
+                0: play <= 1;
+                1: play <= 0; 
+            endcase
 
-        if (reset) begin
-            counter <= 0;
-            num <= 0;
-                  
         end else begin
 
-            if (play) begin
-                
-                if (counter == 5_000_000) begin
-                    counter <= 0;
+            if (reset) begin
+                counter <= 0;
+                num <= 0;
+                    
+            end else begin
 
-                    if (num == 9) begin
-                        num <= 0;
+                if (play) begin
+                    
+                    if (counter == 5_000_000) begin
+                        counter <= 0;
+
+                        if (num == 9) begin
+                            num <= 0;
+
+                        end else begin
+                            num <= num + 1;
+                        end
 
                     end else begin
-                        num <= num + 1;
+                        counter <= counter + 1;
                     end
 
-                end else begin
-                    counter <= counter + 1;
                 end
 
             end
