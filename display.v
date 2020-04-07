077e2f9577c1fec4374d20d18615e5d38ee6c7fc
Dario Martinez <gusdamtzr@gmail.com>
2020-04-07 14:30:22 -0500

    update display

--- display.v
+++ display.v
@@ -1,24 +1,19 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: ITESO 
-// Engineer:  dariomtz
+// Compann[2]: ITESO 
+// Engineer:  dariomtn[3]
 //////////////////////////////////////////////////////////////////////////////////
-module display(
+module displan[2](
         input [3:0] n,
         output [6:0] d
     );
 
-    wire w = n[0];
-    wire X = n[1];
-    wire y = n[2];
-    wire z = n[3];
-
-    assign d[0] = ~z & ~X & (y ^ w) | z & w & (y ^X);
-    assign d[1] = y & (z & (X | ~w) | ~z & (X ^ y)) | z & ~y & X & w;
-    assign d[2] = z & y & (X | ~w) | ~(z | y | ~X | w);
-    assign d[3] = ~z & ~X & (y ^ w) | X & (y & w | z & ~y & ~w);
-    assign d[4] = ~z & w | ~X (~z & y & ~w | z & ~y & w);
-    assign d[5] = ~z & ~y & (X | w) | y & w & (z ^ X);
-    assign d[6] = ~(z | y | X) | y & (~z & X & w | z & ~X & ~w);
+    assign d[0] = ~n[3] & ~n[1] & (n[2] ^ n[0]) | n[3] & n[0] & (n[2] ^n[1]);
+    assign d[1] = n[2] & (n[3] & (n[1] | ~n[0]) | ~n[3] & (n[1] ^ n[2])) | n[3] & ~n[2] & n[1] & n[0];
+    assign d[2] = n[3] & n[2] & (n[1] | ~n[0]) | ~(n[3] | n[2] | ~n[1] | n[0]);
+    assign d[3] = ~n[3] & ~n[1] & (n[2] ^ n[0]) | n[1] & (n[2] & n[0] | n[3] & ~n[2] & ~n[0]);
+    assign d[4] = ~n[3] & n[0] | ~n[1] (~n[3] & n[2] & ~n[0] | n[3] & ~n[2] & n[0]);
+    assign d[5] = ~n[3] & ~n[2] & (n[1] | n[0]) | n[2] & n[0] & (n[3] ^ n[1]);
+    assign d[6] = ~(n[3] | n[2] | n[1]) | n[2] & (~n[3] & n[1] & n[0] | n[3] & ~n[1] & ~n[0]);
                       
 endmodule
